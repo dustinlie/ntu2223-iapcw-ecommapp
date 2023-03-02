6e8f4778b025b33e080bed08d3e653d4180036cb
Dustin Natagatha Sumartlie <T0286006@my.ntu.ac.uk>
2023-03-02 16:00:38 +0000

    add categories page

---
+++ Pages/categories.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace iapCoursework2.Pages
+{
+    public class categoriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
