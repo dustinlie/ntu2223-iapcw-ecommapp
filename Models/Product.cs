e919dbb318ed521ca0a91a065613b1de750e24d4
Dustin Natagatha Sumartlie <T0286006@my.ntu.ac.uk>
2023-03-03 11:47:40 +0000

    add products page and class

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace iapCoursework2.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ProductImg { get; set; }
+
+    }
+}
