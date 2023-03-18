4fef0897d7fc9b343adefd561504d5d0fd5cca98
Dustin Natagatha Sumartlie <T0286006@my.ntu.ac.uk>
2023-03-18 16:10:52 +0000

    add ASP.NET login/register

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using iapCoursework2.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,11 +8,20 @@ builder.Services.AddRazorPages();
 var Configuration = builder.Configuration;
 builder.Services.AddDbContext<AppDataContext>(options => options.UseSqlite(Configuration.GetConnectionString("Default")));
 
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+.AddDefaultTokenProviders()
+.AddDefaultUI()
+.AddEntityFrameworkStores<AppDataContext>();
+
 var app = builder.Build();
 
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication(); 
+app.UseAuthorization();
+
 app.MapRazorPages();
 
 app.Run();
